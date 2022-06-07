56ac4538a6db139e3973de5dd52393ed5459810a
ㅇㅇ <strikers1999@naver.com>
2022-06-08 01:39:54 +0900

    추가

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 console.log("데이터를 읽는 중입니다..");
-const year = 2016;
-const month = "12";
+const year = 2017;
+const month = "05";
 const excelFile = xlsx.readFile(`data/${year}${month}.xlsx`);
 const sheetName = excelFile.SheetNames[0]; // @details 첫번째 시트 정보 추출
 const firstSheet = excelFile.Sheets[sheetName]; // @details 시트의 제목 추출
@@ -29,12 +29,18 @@ let allCount = await db.collection("companies").count();
 
 console.log("데이터를 가공하는 중입니다..");
 const newData = jsonData.map((item) => {
+  const monthSalary = Number(
+    item.pay === 0 ? 0 : ((item.pay / item.total / 9) * 100).toFixed(0)
+  );
+  const yearSalary = monthSalary * 12;
   return {
     title: item.title ? item.title.trim() : "",
     address: item.address ? item.address.trim() : "",
     roadAddress: item.roadAddress ? item.roadAddress.trim() : "",
     code: item.code ? item.code : "",
     codeName: item.codeName ? item.codeName : "",
+    monthSalary,
+    yearSalary,
     info: [
       {
         year,
@@ -42,14 +48,8 @@ const newData = jsonData.map((item) => {
         totalEmployer: item.total,
         joinEmployer: item.join,
         leaveEmployer: item.leave,
-        monthSalary: Number(
-          item.pay === 0 ? 0 : ((item.pay / item.total / 9) * 100).toFixed(0)
-        ),
-        yearSalary: Number(
-          item.pay === 0
-            ? 0
-            : ((item.pay / item.total / 9) * 100 * 12).toFixed(0)
-        ),
+        monthSalary,
+        yearSalary,
       },
     ],
     created: moment().format("YYYY-MM-DD HH:mm:ss"),
@@ -57,45 +57,45 @@ const newData = jsonData.map((item) => {
   };
 });
 
-const salaryTop100 = async () => {
-  console.log("연봉 순으로 내림차순 정렬 합니다.");
-  newData.sort(function (a, b) {
-    // yearSalary 기준으로 정렬
-    if (a.info[0].yearSalary > b.info[0].yearSalary) {
-      return -1;
-    }
-    if (a.info[0].yearSalary < b.info[0].yearSalary) {
-      return 1;
-    }
-    return 0;
-  });
+// const salaryTop100 = async () => {
+//   console.log("연봉 순으로 내림차순 정렬 합니다.");
+//   newData.sort(function (a, b) {
+//     // yearSalary 기준으로 정렬
+//     if (a.info[0].yearSalary > b.info[0].yearSalary) {
+//       return -1;
+//     }
+//     if (a.info[0].yearSalary < b.info[0].yearSalary) {
+//       return 1;
+//     }
+//     return 0;
+//   });
 
-  console.log("벌크 정보를 구성합니다.");
-  const insertBulk = newData.map((item, index) => {
-    return {
-      insertOne: {
-        document: {
-          _id: index + 1,
-          title: item.title ? item.title.trim() : "",
-          address: item.address ? item.address.trim() : "",
-          roadAddress: item.roadAddress ? item.roadAddress.trim() : "",
-          code: item.code ? item.code : "",
-          codeName: item.codeName ? item.codeName : "",
-          year,
-          month,
-          totalEmployer: item.info[0].totalEmployer,
-          joinEmployer: item.info[0].joinEmployer,
-          leaveEmployer: item.info[0].leaveEmployer,
-          monthSalary: item.info[0].monthSalary,
-          yearSalary: item.info[0].yearSalary,
-          created: moment().format("YYYY-MM-DD HH:mm:ss"),
-          updated: moment().format("YYYY-MM-DD HH:mm:ss"),
-        },
-      },
-    };
-  });
-  await companyBySalary.bulkWrite(insertBulk);
-};
+//   console.log("벌크 정보를 구성합니다.");
+//   const insertBulk = newData.map((item, index) => {
+//     return {
+//       insertOne: {
+//         document: {
+//           _id: index + 1,
+//           title: item.title ? item.title.trim() : "",
+//           address: item.address ? item.address.trim() : "",
+//           roadAddress: item.roadAddress ? item.roadAddress.trim() : "",
+//           code: item.code ? item.code : "",
+//           codeName: item.codeName ? item.codeName : "",
+//           year,
+//           month,
+//           totalEmployer: item.info[0].totalEmployer,
+//           joinEmployer: item.info[0].joinEmployer,
+//           leaveEmployer: item.info[0].leaveEmployer,
+//           monthSalary: item.info[0].monthSalary,
+//           yearSalary: item.info[0].yearSalary,
+//           created: moment().format("YYYY-MM-DD HH:mm:ss"),
+//           updated: moment().format("YYYY-MM-DD HH:mm:ss"),
+//         },
+//       },
+//     };
+//   });
+//   await companyBySalary.bulkWrite(insertBulk);
+// };
 
 async function run() {
   try {
@@ -146,6 +146,8 @@ async function run() {
               update: {
                 $set: {
                   info: prevObject.info,
+                  monthSalary: nextItem.monthSalary,
+                  yearSalary: nextItem.yearSalary,
                   updated: moment().format("YYYY-MM-DD HH:mm:ss"),
                 },
               },
@@ -186,14 +188,14 @@ async function run() {
   }
 }
 
-async function run2() {
-  try {
-    await salaryTop100();
-  } finally {
-    console.log(`${year}/${month} 연봉순으로 적재가 끝났습니다.`);
-    await client.close();
-  }
-}
+// async function run2() {
+//   try {
+//     await salaryTop100();
+//   } finally {
+//     console.log(`${year}/${month} 연봉순으로 적재가 끝났습니다.`);
+//     await client.close();
+//   }
+// }
 
 run();
 // run2();
